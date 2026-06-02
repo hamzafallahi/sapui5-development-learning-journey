358e6475dbbdf418495042927aa4f3d839bcb376
Hamza Fellahi <hamza.fellahi@dxc.com>
2026-06-02 10:53:33 +0000

    test

--- webapp/index.js
+++ webapp/index.js
@@ -2,5 +2,5 @@
 sap.ui.define( function (Text) { // and add ["sap/m/Text"],
     "use strict";
 
-    return new sap.m.Text({ text: "Hello World" }).placeAt("content"); //or remove sap.m.
+    return new sap.m.Text({ text: "Hamza World" }).placeAt("content"); //or remove sap.m.
 });
