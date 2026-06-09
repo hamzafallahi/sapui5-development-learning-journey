54cb0994456c57417caf5c24a8b5c4c90d7b21f4
Hamza Fellahi <hamza.fellahi@dxc.com>
2026-06-09 15:51:06 +0000

    Sorting and Filtering

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,12 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/syncStyleClass",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, syncStyleClass, JSONModel) {
+    function (Controller, syncStyleClass, JSONModel, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
@@ -39,9 +42,18 @@ sap.ui.define([
             },
             onCloseDialog: function () {
                 this.byId("dialog").close();
+            },
+            onFilterCustomers: function (oEvent) {
+                var aFilter = [];
+                var sQuery = oEvent.getParameter("query");
+
+                if (sQuery && sQuery.length > 0) {
+                    aFilter.push(new Filter("CustomerName", FilterOperator.Contains, sQuery));
+                }
+
+                var oTable = this.byId("customerTable");
+                var oBinding = oTable.getBinding("items");
+                oBinding.filter(aFilter);
             }
-
-
-
         });
     });
